fd72168de9f7508b20bc9c5873918991f96ece1a
leocij <leocij@hotmail.com>
2017-08-03 16:49:50 -0300

    Criei entidade entrada

---
+++ src/main/java/com/controle/v1/ControleV1Application.java
@@ -0,0 +1,12 @@
+package com.controle.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleV1Application.class, args);
+	}
+}
